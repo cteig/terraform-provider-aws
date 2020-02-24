44ee2bd3dd3955db4e7ead185454a36616439fbf
a504639 <christine.teig@fremtind.no>
2020-02-24 22:16:26 +0100

    plugin

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/terraform-providers/terraform-provider-aws/aws"
+	//"github.com/terraform-providers/terraform-provider-aws/aws"
+	"github.com/hashicorp/terraform/plugin"
 )
 
 func main() {
